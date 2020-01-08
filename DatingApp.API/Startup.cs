813416fceb92d8e7ea48c9b6775ae319633724ec
Madhur Kapoor <madhur.kapoor@gmail.com>
2020-01-08 06:13:24 +0530

    Added UsersController

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -36,7 +36,10 @@ namespace DatingApp.API
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite
                     (Configuration.GetConnectionString("DefaultConnection")));
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson(x => {
+                x.SerializerSettings.ReferenceLoopHandling = 
+                    Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            });
             services.AddCors();
             services.AddScoped<IAuthRepository, AuthRepository>();
             services.AddScoped<IDatingRepository, DatingRepository>();
